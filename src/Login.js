655bf83a7c95f55590e8be7148d2beb811d9f2fc
Ali Ibrahim <alialchi07@gmail.com>
2023-06-17 17:18:42 -0500

    Commit One

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+
+const AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=b2e7f35b30814a369e02eeefb9154735&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <a className="btn btn-success btn-lg" href={AUTH_URL}>
+        Login with Spotify
+      </a>
+    </Container>
+  );
 }
